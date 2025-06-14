7192e309dd741ada88a957a2b3871eb5da4b5af5
JulianRaso <julianraso@live.com>
2025-06-13 21:42:12 -0300

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 import Authentication from "./routes/Authentication";
